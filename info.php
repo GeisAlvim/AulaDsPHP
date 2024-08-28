ff28a3535610db72598f07d49f89e351e97bec26
Geisa Alvim <geisa.alvim@gmail.com>
2024-08-28 18:55:48 -0300

    Initial commit

---
+++ info.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Info</title>
+</head>
+<body>
+    <?php
+        echo "<h1> Olá Mundo </h1>";
+        echo "<h2> oii </h2>";
+        $nome = "Geisa";
+        $altura = 1.70;
+        $idade = 36;
+        
+        echo "Eu sou $nome tem $altura m, sua idade é $idade anos <br><br>";
+
+
+        $linguagem = "PHP";
+        echo 'Programar em $linguagem é muito legal <br>';
+        echo "Programar em $linguagem é muito legal <br>";
+
+    ?>
+</body>
+</html>
